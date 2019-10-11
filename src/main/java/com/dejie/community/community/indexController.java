893adec7bea0375caf3e66d2f28e094cf923cd56
dejie kong <kdj461542718@gmail.com>
2019-10-11 22:51:28 +0800

    add index and bootstrp

---
+++ src/main/java/com/dejie/community/community/indexController.java
@@ -0,0 +1,12 @@
+package com.dejie.community.community;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String index()  { return  "index";}
+}
