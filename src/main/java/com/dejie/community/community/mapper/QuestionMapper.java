5263fe3d0527e34706e597682ff04407848f191d
dejie kong <kdj461542718@gmail.com>
2019-10-24 17:33:54 +0800

    wei wan dai xu

---
+++ src/main/java/com/dejie/community/community/mapper/QuestionMapper.java
@@ -0,0 +1,11 @@
+package com.dejie.community.community.mapper;
+
+import com.dejie.community.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into QUESTION (title,description,gmt_create,gmt_modified,comment_count,view_count,like_count,tag) values (#{title},#{description},#{gmtCreat},#{gmtModified},#{commentCount},#{viewCount},#{likeCount},#{tag})")
+  void creat(Question question);
+}
