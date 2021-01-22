7908a3d05fcf6f7224609c06cff62ae3d59a6e62
461542718 <461542718@qq.com>
2021-01-22 20:52:13 +0800

    P49

--- src/main/resources/db/migration/V4__Create_comment_table_1.sql
+++ src/main/resources/db/migration/V4__Create_comment_table_1.sql
@@ -1,10 +1,12 @@
 create table comment
 (
-	id bigint auto_increment primary  key,
+	id int auto_increment primary  key,
 	parent_id int not null,
 	type int not null,
 	commentator int not null,
 	gmt_create bigint not null,
 	gmt_modified bigint not null,
 	like_count bigint default 0,
+	content varchar(1024),
+	comment_count int default 0
 );
